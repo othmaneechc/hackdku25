1a5c3ba6581665e18d5bf9c66e05e158dc39c358
othmaneechc <echchaabiothmane@gmail.com>
2025-04-23 09:46:40 +0800

    Update page.tsx

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -78,19 +78,19 @@ export default function TimelinePage() {
                 <td className="px-4 py-2 border">Joong‑Tak Yoon, Postdoctoral Research Fellow @ DKU</td>
               </tr>
               <tr>
-                <td className="px-4 py-2 border">15:00 - 16:00</td>
+                <td className="px-4 py-2 border">15:00 - 15:30</td>
                 <td className="px-4 py-2 border">Fintech workshop: Financial Intelligence</td>
                 <td className="px-4 py-2 border">IB Lecture Hall</td>
                 <td className="px-4 py-2 border">Liang Guo, Co-founder @ Guoshu (Huzhou) Intelligent Technology Co., Ltd.</td>
               </tr>
               <tr>
-                <td className="px-4 py-2 border">16:00 - 17:00</td>
+                <td className="px-4 py-2 border">15:30 - 16:00</td>
                 <td className="px-4 py-2 border">Fintech workshop: Agentic Finance</td>
                 <td className="px-4 py-2 border">IB Lecture Hall</td>
                 <td className="px-4 py-2 border">Luyao Zhang, Assistant Professor of Economics @ DKU</td>
               </tr>
               <tr>
-                <td className="px-4 py-2 border">17:00 - 18:00</td>
+                <td className="px-4 py-2 border">16:00 - 18:00</td>
                 <td className="px-4 py-2 border">Hacking continues...</td>
                 <td className="px-4 py-2 border">IB Team Rooms</td>
                 <td className="px-4 py-2 border">Teams work on their projects</td>
