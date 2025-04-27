889381b130967cefd3e0533af612b9f31ec95b1f
othmaneechc <echchaabiothmane@gmail.com>
2025-04-27 17:25:28 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -294,7 +294,7 @@ export default function Home() {
 
       {/* Countdown to hackathon */}
       <div className="container max-w-5xl mx-auto mb-4 text-center">
-        <h2 className="text-3xl font-bold mb-4">The HACKDKU Judging Session ends in</h2>
+        <h2 className="text-3xl font-bold mb-4"></h2>
         {/* Adjust start time here */}
         <Countdown target="2025-04-27T17:00:00" />
       </div>
